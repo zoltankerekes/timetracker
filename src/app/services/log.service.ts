d8474f4c50e4105f1f2a41427d97f529e2f75ed1
Zoltan Kerekes <zoltan.kerekes@betssongroup.com>
2017-07-21 11:18:54 +0200

    Added summary field

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -3,11 +3,24 @@ import { Logmodel } from '../logmodel';
 
 @Injectable()
 export class LogService {
+  workSecondsCounter: number = 0;
+  breakSecondsCounter: number = 0;
+  totalWork: number = 0;
+  totalBreak: number = 0;
   logs: Logmodel[] = [];
   constructor() { 
   }
 
   logActivity(startDate: Date, activityName: string, durationInSeconds: number){
     this.logs.unshift(new Logmodel(startDate, activityName, durationInSeconds));
+    if (activityName === "Work"){
+      this.workSecondsCounter = this.workSecondsCounter + durationInSeconds;
+      this.totalWork = new Date(0,0,0,0,0,0,0).setSeconds(this.workSecondsCounter);
+    }
+    else {
+      this.breakSecondsCounter = this.breakSecondsCounter + durationInSeconds;
+      this.totalBreak = new Date(0,0,0,0,0,0,0).setSeconds(this.breakSecondsCounter + durationInSeconds);
+    }
   }
+
 }
